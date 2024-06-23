830c50bc681a6868f99c79762591f79d6e93d466
Joyce Lau <jshizuki@yahoo.com.hk>
2024-06-23 11:10:15 +0900

    Complete min_window_substring

--- ruby/min_window_substring.rb
+++ ruby/min_window_substring.rb
@@ -1,39 +1,66 @@
-def min_window_substring(strArr)
+def min_window_substring(str)
+  first = str[0].chars
+  second = str[1].chars
 
-  p first_string = strArr[0].chars
-  p second_string = strArr[1].chars
+  first_count = Hash.new
+  second_count = Hash.new
 
-  character_count = Hash.new
-
-  first_string.each do |char|
-    character_count[char].nil? ?
-    character_count[char] = 1 : character_count[char] += 1
+  first.each_with_index do |char, index|
+    first_count[char].nil? ? first_count[char] = [index] : first_count[char] << index
   end
 
-  single_occurrence_chars = character_count.filter do |key, value|
-    value == 1 && second_string.include?(key)
+  second.each do |char|
+    second_count[char].nil? ? second_count[char] = 1 : second_count[char] += 1
   end
 
-  multiple_occurrence_chars = character_count.filter do |key, value|
-    value != 1 && second_string.include?(key)
+  # p first_count
+  # p second_count
+  min_window = []
+  min_window_length = Float::INFINITY
+  potential_window = []
+
+  left, right = 0
+
+  window_count = Hash.new
+
+  until second_count.any? { |key, value|
+    !window_count.empty? &&
+      window_count[key] < value && first_count[key].count < second_count[key]
+  }
+    second.each do |char|
+      if !potential_window.include?(char)
+        right = first_count[char].first
+        potential_window = first[left..right]
+      end
+    end
+
+    second.each do |char|
+      window_count[char] = potential_window.count(char)
+    end
+
+    potential_window_length = potential_window.length
+    potential_window_length <= min_window_length ? min_window = potential_window : min_window
+
+    # p min_window
+
+    common_characters = potential_window.each.select { |each| second.include?(each) }
+
+    first_character = common_characters[0]
+
+    first_count[first_character].shift
+    window_count[first_character] -= 1
+    second_character_index = first_count[common_characters[1]].first
+    left = second_character_index
+    potential_window = first[left..right]
   end
 
-  indexes_of_min_window = []
-
-  single_occurrence_chars.keys.each do |char|
-    indexes_of_min_window << first_string.index(char)
-  end
-
-  p single_occurrence_chars
-  p multiple_occurrence_chars
-  p indexes_of_min_window
-
-  # min_window
-
+  min_window.join("")
 end
 
-# p min_window_substring(["ahffaksfajeeubsne", "jefaab"])
-# #Output: aksfajeeub
+p min_window_substring(["ahffaksfajeeubsne", "jefaab"])
+# Output: aksfajeeub
 
-min_window_substring(["aaff hkksem fkelloe", "fheam"])
+# p min_window_substring(["aaffhkksemfkelloe", "fheam"])
 # Output: affhkksem
+
+# p min_window_substring(["ADOBECODEBANC", "ABC"])
